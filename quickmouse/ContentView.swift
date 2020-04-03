e0d1d1af2e24271868ca687d4f227835ace21ea0
Bryce Hanscomb <b.hanscomb@gmail.com>
2020-04-03 14:50:07 +1100

    Initial Commit

---
+++ quickmouse/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  quickmouse
+//
+//  Created by Bryce Hanscomb on 3/4/20.
+//  Copyright © 2020 STAK Digital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
