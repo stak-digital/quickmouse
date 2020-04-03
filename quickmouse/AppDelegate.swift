c244a3c870eb1a8e116bc402b86ff107f597aaf6
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-04-03 23:17:28 +1100

    get the click functionality working

--- quickmouse/AppDelegate.swift
+++ quickmouse/AppDelegate.swift
@@ -10,6 +10,8 @@ import Cocoa
 import SwiftUI
 import ApplicationServices
 
+//import "ClickAction.h"
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
@@ -28,14 +30,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     ]
     
     var window: NSWindow!
-    
+    var highlightedCell = 5
     
     func myCallback(_ evt: NSEvent) {
         
         let keyCode = Int(evt.keyCode)
         let currentSize = self.window.frame.size
         let frame = self.window.frame
-                
+        
         var rect: NSRect;
         
         let newWidth = currentSize.width / 3
@@ -53,35 +55,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         case self.keyCodeDict["NUMPAD_9"]:
             rect = NSRect(x: frame.minX + newWidth * 2, y: (((frame.maxY - frame.minY) / 3) * 2) + frame.minY, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_4"]:
             rect = NSRect(x: frame.minX, y: frame.minY + newHeight, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_5"]:
             rect = NSRect(x: frame.minX + newWidth, y: frame.minY + newHeight, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_6"]:
             rect = NSRect(x: frame.minX + newWidth * 2, y: frame.minY + newHeight, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_1"]:
             rect = NSRect(x: frame.minX, y: frame.minY, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_2"]:
             rect = NSRect(x: frame.minX + newWidth, y: frame.minY, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["NUMPAD_3"]:
             rect = NSRect(x: frame.minX + newWidth * 2, y: frame.minY, width: newWidth, height: newHeight)
             self.resizeWindow(newFrame: rect)
-        break
+            break
         case self.keyCodeDict["RETURN"]:
-            self.click(x: Int(frame.midX), y: Int(frame.maxY - frame.minY - 20 - (newHeight / 3))) // click at the middle of the middle cell
-            exit(0) // quit the application
-        break
+            self.window.close() // this is slow to close, so we need to add a delay before we trigger click
+            // or else it'll just click our own app window
+            
+            let screenSize = CGDisplayBounds(CGMainDisplayID())
+            
+            let clickPos: NSPoint = NSPoint(
+                x: frame.midX,
+                y: screenSize.height - frame.midY
+            ) // click at the middle of the middle cell
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // Change `2.0` to the desired number of seconds.
+                self.click(
+                    x: Int(clickPos.x),
+                    y: Int(clickPos.y)
+                ) // click at the middle of the middle cell
+                
+                self.click(
+                    x: Int(clickPos.x),
+                    y: Int(clickPos.y)
+                ) // click at the middle of the middle cell
+                
+                exit(0)
+            }
+            break
         default:
             let alert = NSAlert.init()
             alert.messageText = "Unsupported Keypress"
@@ -124,7 +147,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.window.setFrame(newFrame, display: true)
     }
     
+    func postMouseEvent(button: CGMouseButton, type: CGEventType, point: CGPoint) {
+        if let theEvent: CGEvent = CGEvent(mouseEventSource: nil, mouseType: type, mouseCursorPosition: point, mouseButton: button) {
+            theEvent.type = type;
+            theEvent.post(tap: .cghidEventTap);
+        } else {
+            print("I dunno")
+        }
+        
+    }
+    
+    
     func click(x: Int, y: Int) {
         print("click at " + String(x) + "x" + String(y))
+        
+        let position = CGPoint(x: x, y: y)
+        
+        let source = CGEventSource.init(stateID: .hidSystemState)
+        let eventDown = CGEvent(mouseEventSource: source, mouseType: .leftMouseDown, mouseCursorPosition: position , mouseButton: .left)
+        let eventUp = CGEvent(mouseEventSource: source, mouseType: .leftMouseUp, mouseCursorPosition: position , mouseButton: .left)
+        
+        if let eDown = eventDown {
+            /*
+              These events will never actually be received by the system if there has been a code change
+              since the last build and you haven't toggled the Mac OS System Preferences -> Security & Privacy -> Accessibility
+              checkbox for this app. It will fail silently unless you uncheck it and re-check it again once per code change.
+             
+              Also these events require the App Sandbox to be disabled.
+             */
+            eDown.post(tap: .cghidEventTap)
+        } else {
+            print("Click didn't happen")
+        }
+        
+        usleep(500_000)
+        
+        
+        if let eUp = eventUp {
+            eUp.post(tap: .cghidEventTap)
+        } else {
+            print("Click didn't happen")
+        }
     }
 }
---
+++ quickmouse/ClickAction-Bridging-Header.h
@@ -0,0 +1,11 @@
+////
+////  ClickAction-Bridging-Header.m
+////  quickmouse
+////
+////  Created by Bryce Hanscomb on 3/4/20.
+////  Copyright © 2020 STAK Digital. All rights reserved.
+////
+//
+//#import <Foundation/Foundation.h>
+//
+//#import "ClickAction.h"
