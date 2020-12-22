7ef42a0acbaa4278268c2979859aae72c753a83c
Bryce Hanscomb <b.hanscomb@gmail.com>
2020-12-22 14:17:34 +1100

    cleanup

--- quickmouse/AppDelegate.swift
+++ quickmouse/AppDelegate.swift
@@ -29,24 +29,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let statusBarMenu = NSMenu(title: "Cap Status Bar Menu")
 
         statusBarItem.menu = statusBarMenu
-        
+
         statusBarMenu.addItem(
             withTitle: "Show QuickMouse",
             action: #selector(handleShowFromMenuRequested),
             keyEquivalent: ""
         )
-        
+
         statusBarMenu.addItem(
             withTitle: "Quit QuickMouse",
             action: #selector(handleQuitFromMenuRequested),
             keyEquivalent: ""
         )
     }
-    
+
     func quit() {
         exit(0)
     }
-    
+
     func handleKeyUp(_ evt: NSEvent) {
 
     }
@@ -65,7 +65,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         grid.centerHighlight()
         renderWindow()
     }
-    
+
     func removeOldestEventFromFlagChangePool() -> Void {
         /*
          * need this check because if the async queuing of this fn ends up calling when
@@ -75,29 +75,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             flagChangePool.remove(at: 0)
         }
     }
-    
+
     func hideWindow() {
         // https://stackoverflow.com/a/48353056/1063035
         // hides the window. This is necessary because `window.orderOut` doesn't give focus to the next-underneath window.
         NSApp.hide(self)
     }
-    
+
     @objc func showWindow() {
         NSApp.unhide(self)
         NSApp!.activate(ignoringOtherApps: true)
         window.makeKeyAndOrderFront(self)
     }
-    
+
     func isWindowShowing() -> Bool {
         NSApp.isHidden
     }
-    
+
     func selectHighlightedCell() {
         grid.zoomInOnHighlightedCell()
         grid.centerHighlight()
         renderWindow()
     }
-    
+
     func undoColSelection() {
         /*
          * If full-size already, we can't go any further, so just close the window.
@@ -111,20 +111,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     // given a list of selected cells, split the screen successively to zoom in each step
-    func calculateWindowSize( grid: GridManager) -> NSRect {
+    func calculateGridSize() -> NSRect {
         var rect: NSRect = WindowManager.getScreenSize()
-        
+
         // TODO: just use a reduce() here
         grid.selectedCells.forEach({ cell in
             rect = WindowManager.getRectForCell(currentRect: rect, whichCell: cell)
         })
-        
+
         return rect
     }
 
     func renderWindow() {
         window.setFrame(
-            WindowManager.convertToBufferFrame(calculateWindowSize(grid: grid)),
+            WindowManager.convertToBufferFrame(calculateGridSize()),
             display: true
         )
     }
@@ -199,27 +199,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         alert.addButton(withTitle: buttonText)
         alert.runModal()
     }
-    
+
     func submit() {
         let screenSize = WindowManager.getScreenSize()
         let frame = window.frame
-        
+
         hideWindow() // this is slow to close, so we need to add a delay before we trigger click
         // or else it'll just click our own app window (or the event just doesn't go through)
-        
+
         let clickPos: NSPoint = NSPoint(
             x: frame.midX,
             y: screenSize.height - frame.midY
         ) // click at the middle of the middle cell
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             MouseManager.click(clickPos)
         }
     }
-    
+
     func handleFlagsChanged(_ event: NSEvent) {
         let isControlKeyDownRightNow = event.modifierFlags.intersection(.deviceIndependentFlagsMask).contains(.control)
-        
+
         if (!isControlKeyDownRightNow) {
             return
         }
@@ -231,13 +231,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         flagChangePool.forEach({ evt in
             let isControlKeyDown = evt.modifierFlags.intersection(.deviceIndependentFlagsMask).contains(.control)
-            
+
             if (isControlKeyDown) {
                 eventsWithControlKeyDown.append(evt)
             }
         })
-        
-        
+
+
         if (eventsWithControlKeyDown.count >= 2) {
             /*
              * Unhide
@@ -249,7 +249,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             } else {
                 hideWindow()
             }
-            
+
             // flush them all so that if the user presses it one more time, there won't be maybe 3 valid events in the time window
             // causing only one more press to toggle again
             // TODO: cancel all queued deletion calls from below!
@@ -262,22 +262,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
-    
+
     func listenForGlobalHotKey() {
         hotKeyMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.flagsChanged], handler: handleFlagsChanged)
     }
-    
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView().environmentObject(grid).background(Color.clear)
-        
+
         // The application does not appear in the Dock and does not have a menu
         // bar, but it may be activated programmatically or by clicking on one
         // of its windows.
         NSApp!.setActivationPolicy(.accessory)
-        
+
         setupMenuBarIcon()
-        
+
         // Create the window and set the content view.
         window = MyWindow(
             contentRect: WindowManager.getScreenSize(),
@@ -288,7 +288,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             keyUpHandler: handleKeyUp,
             flagsChangeHandler: handleFlagsChanged
         )
-        
+
         window.setFrameAutosaveName("Main Window")
         window.contentView = NSHostingView(rootView: contentView)
         window.isOpaque = false
@@ -300,51 +300,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func getCenterPositionOfHighlightedCell() -> NSPoint {
-        let gridContainer: NSRect = calculateWindowSize(grid: grid)
-
-        let cellWidth = gridContainer.width / 3
-        let cellHeight = gridContainer.height / 3
-
-        let halfCellWidth = cellWidth / 2
-        let halfCellHeight = cellHeight / 2
-
-        let firstColMiddleX = gridContainer.minX// + halfCellWidth
-        let firstRowMiddleY = gridContainer.minY// + halfCellHeight
-
-        var middleX = firstColMiddleX
-        var middleY = firstRowMiddleY
-
-        switch grid.highlightedCell {
-            case 1,4,7:
-                middleX = middleX + (cellWidth * 0) + halfCellWidth
-                break
-            case 2,5,8:
-                middleX = middleX + (cellWidth * 1) + halfCellWidth
-                break
-            case 3,6,9:
-                middleX = middleX + (cellWidth * 2) + halfCellWidth
-            default:
-                break
-        }
-
-        switch grid.highlightedCell {
-            // top row
-            case 7,8,9:
-                middleY = middleY + (cellHeight * 0) + halfCellHeight
-                break
-            // center row
-            case 4,5,6:
-                middleY = middleY + (cellHeight * 1) + halfCellHeight
-                break
-            // bottom row
-            case 1,2,3:
-                middleY = middleY + (cellHeight * 2) + halfCellHeight
-                break
-            default:
-                break
-            }
-
-        return NSPoint(x: middleX, y: middleY)
+        let gridContainer: NSRect = WindowManager.getRectForCell(
+            currentRect: calculateGridSize(),
+            whichCell: grid.highlightedCell
+        )
+        return NSPoint(x: gridContainer.midX, y: gridContainer.midY)
     }
 
     func moveMouseToHighlightedCell() {
--- quickmouse/MyWindow.swift
+++ quickmouse/MyWindow.swift
@@ -1,11 +1,3 @@
-//
-//  MyWindow.swift
-//  quickmouse
-//
-//  Created by Bryce Hanscomb on 3/4/20.
-//  Copyright © 2020 STAK Digital. All rights reserved.
-//
-
 import Foundation
 import Cocoa
 
@@ -24,32 +16,28 @@ class MyWindow: NSWindow {
         keyUpHandler: @escaping (_ evt: NSEvent) -> Void,
         flagsChangeHandler: @escaping (_ evt: NSEvent) -> Void
     ) {
-        self.handleKeyDown = keyDownHandler
-        self.handleKeyUp = keyUpHandler
-        self.handleFlagsChange = flagsChangeHandler
+        handleKeyDown = keyDownHandler
+        handleKeyUp = keyUpHandler
+        handleFlagsChange = flagsChangeHandler
 
         super.init(contentRect: contentRect, styleMask: styleMask, backing: backing, defer: shouldDefer)
-        
     }
     
     override func keyDown(with: NSEvent) {
-//        super.keyDown(with: with)
-        self.handleKeyDown(with)
+        handleKeyDown(with)
     }
     
     override func keyUp(with event: NSEvent) {
-//        super.keyUp(with: event)
-        self.handleKeyUp(event)
+        handleKeyUp(event)
     }
     
     override func flagsChanged(with event: NSEvent) {
-//        super.flagsChanged(with: event)
-        self.handleFlagsChange(event)
+        handleFlagsChange(event)
     }
     
     // https://stackoverflow.com/a/8284133
-    override var canBecomeKey: Bool { get { return true }}
+    override var canBecomeKey: Bool { get { true }}
     
-    override var isReleasedWhenClosed: Bool { get { return false } set {  } }
+    override var isReleasedWhenClosed: Bool { get { false } set {  } }
     
 }
